cfa86cf7d41413618ade72c1bfe132ef6c077978
Bart Massey <bart.massey@gmail.com>
2023-07-24 01:14:56 -0700

    fixed initialization; works now

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![no_std]
 
 use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::{rprint, rprintln, rtt_init_print};
 
 use cortex_m_rt::entry;
 use microbit::{
@@ -24,16 +24,23 @@ fn main() -> ! {
     let mut timer = Timer::new(board.TIMER0);
     let mut lsm303 = Lsm303agr::new_with_i2c(i2c);
     lsm303.init().unwrap();
+    lsm303.set_accel_mode_and_odr(
+        &mut timer,
+        lsm303agr::AccelMode::Normal,
+        lsm303agr::AccelOutputDataRate::Hz1,
+    ).unwrap();
+
     loop {
         let status = lsm303.temperature_status().unwrap();
-        if status.overrun() {
-            rprintln!("overrun");
-        }
-        if status.new_data() {
-            rprintln!("new data");
-        }
         let deg_c = lsm303.temperature().unwrap().degrees_celsius();
-        rprintln!("{}", deg_c * 9.0 / 5.0 + 32.0);
+        rprint!("{}", deg_c * 9.0 / 5.0 + 32.0);
+        if status.overrun() {
+            rprint!(" (overrun)");
+        }
+        if !status.new_data() {
+            rprint!(" (old data)");
+        }
+        rprintln!();
         timer.delay_ms(1000u16);
     }
 }
